9df85cc1b3840c2a8b3d7da7887422fd98d79129
ujjwals653 <ujjwals653@gmail.com>
2025-03-19 23:50:38 +0530

    Removed Router components as it causing issues with vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,48 +2,16 @@ import React from 'react';
 import MainLayout from './components/layout/MainLayout';
 import SignUpPage from './components/auth/SignUpPage';
 import { SignedIn, SignedOut } from '@clerk/clerk-react';
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/sign-in/*" element={
-            <>
-              <SignedOut>
-                <SignUpPage />
-              </SignedOut>
-              <SignedIn>
-                <Navigate to="/chat" replace />
-              </SignedIn>
-            </>
-          } />
-
-        <Route path="/chat/*" element={
-          <>
-            <SignedIn>
-              <MainLayout/>
-            </SignedIn>
-            <SignedOut>
-              <Navigate to="/sign-in" replace />
-            </SignedOut>
-          </>
-          } />
-
-        {/* Redirect to chat or sign-in based on sing-in status */}
-        <Route path="*" element={
-            <>
-              <SignedIn>
-                <Navigate to="/chat" replace />
-              </SignedIn>
-              <SignedOut>
-                <Navigate to="/sign-in" replace />
-              </SignedOut>
-            </>
-          } />
-      </Routes>
-    </BrowserRouter>
+      <SignedIn>
+        <MainLayout />
+      </SignedIn>
+      <SignedOut>
+        <SignUpPage />
+      </SignedOut>
     </>
   );
 }
